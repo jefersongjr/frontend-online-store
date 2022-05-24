48ac0687868fb6063738b4ad24af9a99c4926de0
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-24 16:56:23 -0300

    requisito 2 , criando input

---
+++ src/components/ProductsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductsConteiner extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default ProductsConteiner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ProductsConteiner from '../components/ProductsContainer';
 
 class Home extends React.Component {
   render() {
     return (
-      <div />
+      <section>
+        <ProductsConteiner />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </section>
     );
   }
 }
