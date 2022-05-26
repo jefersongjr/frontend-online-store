7d1ea3af93fd72647e325bb0025d25deaae63d97
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-26 19:05:04 -0300

    requisito 13 -  criando quantidade de numero

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   render() {
+    const { cartItems } = this.props;
+    let sumTotal = 0;
+    if (cartItems.length !== 0) {
+      const arrayQuantity = cartItems.map((item) => item.quantity);
+      sumTotal = arrayQuantity.reduce((item, acc) => acc + item);
+    }
     return (
-      <Link
-        to="/cart"
-        data-testid="shopping-cart-button"
-      >
-        carrinho de compras
-      </Link>
+      <div>
+        <Link
+          to="/cart"
+          data-testid="shopping-cart-button"
+        >
+          carrinho de compras
+        </Link>
+        <div>
+          <p data-testid="shopping-cart-size">
+            { sumTotal }
+          </p>
+        </div>
+      </div>
     );
   }
 }
+
+Button.propTypes = {
+  cartItems: PropTypes.string.isRequired,
+};
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 
 class Header extends React.Component {
   render() {
+    const { cartItems } = this.props;
     return (
       <header>
-        <Button />
+        <Button cartItems={ cartItems } />
         <Link to="/">Home</Link>
       </header>
     );
   }
 }
+
+Header.propTypes = {
+  cartItems: PropTypes.arrayOf.isRequired,
+};
 
 export default Header;
