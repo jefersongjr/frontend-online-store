289906cfd0d311c61898ae72ded166aeb5c8ea11
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-05-26 15:54:39 -0300

    [Group 20] - Projeto Frontend Online Store

---
+++ src/components/AvaluationBoard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AvaluationCard from './AvaluationCard';
+
+class AvaluationBoard extends React.Component {
+  render() {
+    const { avaluationsSaved } = this.props;
+    return (
+      <section className="avaluations-saved-sec">
+        {avaluationsSaved.map((avaluation, index) => (
+          <AvaluationCard
+            key={ index }
+            description={ avaluation.description }
+            rate={ avaluation.rate }
+            email={ avaluation.email }
+          />
+        ))}
+      </section>
+    );
+  }
+}
+
+AvaluationBoard.propTypes = {
+  avaluationsSaved: PropTypes.func.isRequired,
+};
+
+export default AvaluationBoard;
